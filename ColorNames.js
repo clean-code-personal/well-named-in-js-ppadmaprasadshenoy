7faadb34ad00f0374a9e8bd87348bbd4458414d3
P Padmaprasad Shenoy <92221430+ppadmaprasadshenoy@users.noreply.github.com>
2023-02-20 15:18:27 +0530

    Delete ColorNames.js

--- ColorNames.js
+++
@@ -1,9 +0,0 @@
-
-const MajorColorNames = [
-    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
-];
-const MinorColorNames = [
-    "BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
-];
-
-export { MajorColorNames, MinorColorNames };
