aa8bb33716f07863cff0873ee37a4d2c22b27fdb
P Padmaprasad Shenoy <92221430+ppadmaprasadshenoy@users.noreply.github.com>
2023-02-20 15:12:14 +0530

    Create ColorNames.js

---
+++ ColorNames.js
@@ -0,0 +1,9 @@
+
+const MajorColorNames = [
+    "WHITE", "RED", "BLACK", "YELLOW", "VIOLET"
+];
+const MinorColorNames = [
+    "BLUE", "ORANGE", "GREEN", "BROWN", "SLATE"
+];
+
+export { MajorColorNames, MinorColorNames };
